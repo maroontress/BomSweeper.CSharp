928ad4b218ffb2e6795e0b809a400ca5189fe398
Tomohisa Tanaka <Tomohisa.Tanaka@gmail.com>
2019-05-17 02:45:29 +0900

    Refactor.
    - Add API documents.

--- BomSweeper.Test/PathFinder/ExceptionThrowingDirectoryAct.cs
+++ BomSweeper.Test/PathFinder/ExceptionThrowingDirectoryAct.cs
@@ -5,34 +5,54 @@ namespace BomSweeper.Test.PathFinder
     using System.IO;
     using BomSweeper;
 
-    public class ExceptionThrowingDirectoryAct : DirectoryAct
+    /// <summary>
+    /// A <see cref="DirectoryAct"/> implementation. All the methods throw
+    /// an exception.
+    /// </summary>
+    public sealed class ExceptionThrowingDirectoryAct : DirectoryAct
     {
-        private readonly Action throwingAction;
+        private readonly Func<Exception> newException;
 
+        /// <summary>
+        /// Initializes a new instance of the <see
+        /// cref="ExceptionThrowingDirectoryAct"/> class.
+        /// </summary>
+        /// <param name="newException">
+        /// The function that returns a new exception. Calling any method of
+        /// this class throws the exception this function supplies.
+        /// </param>
+        /// <param name="name">
+        /// The directory's name.
+        /// </param>
+        /// <param name="attributes">
+        /// The directory's attribute.
+        /// </param>
         public ExceptionThrowingDirectoryAct(
-            Action throwingAction,
+            Func<Exception> newException,
             string name,
             FileAttributes attributes = FileAttributes.Directory)
         {
-            this.throwingAction = throwingAction;
+            this.newException = newException;
             Name = name;
             Attributes = attributes;
         }
 
+        /// <inheritdoc/>
         public string Name { get; }
 
+        /// <inheritdoc/>
         public FileAttributes Attributes { get; }
 
+        /// <inheritdoc/>
         public IEnumerable<DirectoryAct> GetDirectories()
         {
-            throwingAction();
-            throw new ApplicationException();
+            throw newException();
         }
 
+        /// <inheritdoc/>
         public IEnumerable<FileAct> GetFiles()
         {
-            throwingAction();
-            throw new ApplicationException();
+            throw newException();
         }
     }
 }
