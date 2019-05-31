d9566d102a41969f372bf8b939520f4d2880d6b0
Tomohisa Tanaka <Tomohisa.Tanaka@gmail.com>
2019-05-31 21:44:26 +0900

    Fix to handle UnauthorizedAccessException.

--- BomSweeper/BomSweeper/BomKit.cs
+++ BomSweeper/BomSweeper/BomKit.cs
@@ -46,6 +46,10 @@ namespace BomSweeper
             {
                 return false;
             }
+            catch (UnauthorizedAccessException)
+            {
+                return false;
+            }
         }
 
         /// <summary>
